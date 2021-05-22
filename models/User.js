240ca161d6adbccc6886c14e41e06e56d011f255
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-05-22 12:56:45 -0600

    user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: 'Email is required',
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
