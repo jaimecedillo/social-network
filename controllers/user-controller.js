86defaa6d3578dc6c414a0a147ee33f3566c9786
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-05-22 14:21:51 -0600

    user routes

---
+++ controllers/user-controller.js
@@ -0,0 +1,84 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUser(re, res) {
+        User.find({})
+            .then(UserData => res.json(UserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .then(UserData => {
+                // If no User is found, send 404
+                if (!UserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(UserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // create user
+    createUser({ body }, res) {
+        User.create({})
+            .then(UserData => res.json(UserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // update user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(UserData => {
+                // If no User is found, send 404
+                if (!UserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(UserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(UserData => {
+                // If no User is found, send 404
+                if (!UserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(UserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+
+};
+
+module.exports = userController;
+
+
+
+
+
+
+
+
+
